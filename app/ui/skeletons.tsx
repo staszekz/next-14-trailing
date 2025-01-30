10d7823bd23283e6d8c30c3a2a7e4228ce410483
Staszek Zajaczkowski <staszek.zajaczkowski@gmail.com>
2025-01-30 14:53:22 +0100

    upgrade tailwind to v4 and create custom fonts

--- app/ui/skeletons.tsx
+++ app/ui/skeletons.tsx
@@ -1,11 +1,11 @@
 // Loading animation
 const shimmer =
-  "before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/60 before:to-transparent";
+  "before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:bg-linear-to-r before:from-transparent before:via-white/60 before:to-transparent";
 
 export function CardSkeleton() {
   return (
     <div
-      className={`${shimmer} relative overflow-hidden rounded-xl bg-gray-100 p-2 shadow-sm`}
+      className={`${shimmer} relative overflow-hidden rounded-xl bg-gray-100 p-2 shadow-xs`}
     >
       <div className="flex p-4">
         <div className="h-5 w-5 rounded-md bg-gray-200" />
